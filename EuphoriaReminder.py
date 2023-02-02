db7e5da7bef8bd5e55419cbb7082f3fafeefc6cc
BitFlippy <de.andreas.klein@gmail.com>
2023-02-02 20:30:01 +0100

    sadf

--- EuphoriaReminder.py
+++ EuphoriaReminder.py
@@ -3,35 +3,21 @@ from win10toast import ToastNotifier
 import random
 import time
 
-MessageList = [
-"You are enough.",
-"You can be proud of who you are.",
-"You are more than worthy of love.",
-"You don't have to compare yourself to others.",
-"You get better every day.",
-"You are worthy of all things wonderful.",
-"Your body deserves to be taken care of.",
-"You are so beautiful, inside and out.",
-"You're not weak when I ask for help.",
-"Your true friends love and support You.",
-"You deserve good things.",
-"Only you can decide who you are.",
-"You can reach your goals if you keep at them.",
-"You're am not alone in your journey.",
-"You belong just as much as everyone else.",
-"You are valid and valued.",
-"You are beautifull",
-"WOW you're actually kinda cute",
-"Time for water!",
-]
+
+
+with open("MessageList.csv", "r") as file:
+    lines = file.read().splitlines()
+    for line in lines:
+        # Process line and create table item
+        print(lines)
 
 
 while (True):
     # random item from list
-    Message=(random.choice(MessageList))
+    Message=(random.choice(lines))
 
     # create an object to ToastNotifier class
     n = ToastNotifier()
-    n.show_toast(Message, "<3", duration = 5,
+    n.show_toast(Message, "<3", duration = 10,
     icon_path ="https://media.geeksforgeeks.org/wp-content/uploads/geeks.ico")
     time.sleep(300)
