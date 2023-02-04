7917c60705505cb8175454e82b64b5ed9c73cf51
BitFlippy <de.andreas.klein@gmail.com>
2023-02-04 12:56:36 +0100

    working version with the web source instead of the local stored list. also doc now supports any format.

--- EuphoriaReminder.py
+++ EuphoriaReminder.py
@@ -1,23 +1,25 @@
-# import win10toast 
-from win10toast import ToastNotifier
 import random
+import requests
 import time
+import win10toast
 
+def get_random_message():
+    # Get the contents of the web source as a string
+    response = requests.get("https://raw.githubusercontent.com/BitFlippy/Euphoria.Reminder/main/MessageList.csv")
+    content = response.content.decode("utf-8")
 
+    # Split the content into lines
+    lines = content.split("\n")
 
-with open("MessageList.csv", "r") as file:
-    lines = file.read().splitlines()
-    for line in lines:
-        # Process line and create table item
-        print(lines)
+    # Choose a random line and return it
+    return random.choice(lines)
 
+toaster = win10toast.ToastNotifier()
 
-while (True):
-    # random item from list
-    Message=(random.choice(lines))
+while True:
+    # Get a random message and show it as a notification without an icon
+    message = get_random_message()
+    toaster.show_toast("Cute Reminder", message, duration=7)
 
-    # create an object to ToastNotifier class
-    n = ToastNotifier()
-    n.show_toast(Message, "<3", duration = 10,
-    icon_path ="https://media.geeksforgeeks.org/wp-content/uploads/geeks.ico")
-    time.sleep(300)
+    # Wait for 5 minutes
+    time.sleep(5 * 60)
